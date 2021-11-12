aaedde81b40bb48f6efe7223f10a3407880c6a8a
Christinamawel <christinamawel@gmail.com>
2021-11-12 11:14:07 -0800

    create eigth test and recive expected fail

--- __tests__/Person.test.js
+++ __tests__/Person.test.js
@@ -36,7 +36,7 @@ describe('Person', () => {
     expect(person.lifeExpectancy()).toEqual(81.4)
   })
 
-  // test('should create Person object with method to find remaining life expectancy for each planet', () => {
-  //   expect(peron.remainingLife()).toEqual(["Earth: 47.4", "Mercury: 73.24", "Venus: 60.32", "Mars: 17.48", "Jupiter: 321.84 over life expectancy"])
-  // })
+  test('should create Person object with method to find remaining life expectancy for each planet', () => {
+    expect(person.remainingLife()).toEqual(["Earth: 47.4", "Mercury: 73.24", "Venus: 60.32", "Mars: 17.48", "Jupiter: 321.84 over life expectancy"])
+  })
 })

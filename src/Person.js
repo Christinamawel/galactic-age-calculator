fcaf462a5556ef883395c9fef9ea191dcf75d339
Christinamawel <christinamawel@gmail.com>
2021-11-12 09:59:25 -0800

    pass fourth test

--- src/Person.js
+++ src/Person.js
@@ -2,7 +2,8 @@ export default class Person {
 
   constructor(age) {
     this.age = age;
-    this.mercuryYears = age * .24;
-    this.venusYears = age * .62;
+    this.mercuryYears = Math.round((age * .24) * 100)/100;
+    this.venusYears = Math.round((age * .62) * 100)/100;
+    this.marsYears = Math.round((age * 1.88) * 100)/100;
   }
 }

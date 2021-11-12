be2452d2bd03a84c0c521d8b091c705fe3cdcccd
Christinamawel <christinamawel@gmail.com>
2021-11-12 10:03:41 -0800

    pass fith test

--- src/Person.js
+++ src/Person.js
@@ -5,5 +5,6 @@ export default class Person {
     this.mercuryYears = Math.round((age * .24) * 100)/100;
     this.venusYears = Math.round((age * .62) * 100)/100;
     this.marsYears = Math.round((age * 1.88) * 100)/100;
+    this.jupiterYears = Math.round((age * 11.86) * 100)/100;
   }
 }
